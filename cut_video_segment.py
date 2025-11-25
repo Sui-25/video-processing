6ede8df9b78b88f2d89c0f514223cfffd7c57dfd
Sui-25 <2310153599@qq.com>
2025-11-25 15:53:52 +0800

    fix

--- cut_video_segment.py
+++ cut_video_segment.py
@@ -1,17 +1,19 @@
 import cv2
 
 # 输入视频路径
-input_path = "videos/stick_swing_exmple.mp4"
-output_path = "videos/stick_swing_20s.mp4"
+input_path = "videos/2025-11-18 19-19-51.mp4"
+output_path = "videos/stick_swing_3_20s.mp4"
 
 # 打开视频文件
 cap = cv2.VideoCapture(input_path)
 
 # 获取视频的基本信息
-fps = cap.get(cv2.CAP_PROP_FPS)              # 帧率 (frames per second)
-width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))   # 视频宽度
-height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)) # 视频高度
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')     # 视频编码器 (mp4 常用 mp4v/avc1/H264)
+fps = cap.get(cv2.CAP_PROP_FPS)  # 帧率 (frames per second)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))  # 视频宽度
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # 视频高度
+fourcc = cv2.VideoWriter.fourcc(
+    *"mp4v"
+)  # 视频编码器 (mp4 常用 mp4v/avc1/H264)
 
 # 输出视频写入器
 out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
@@ -23,12 +25,14 @@ frame_count = 0
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
+        print("读取失败")
         break  # 读取失败，可能是视频结束
-    
-    out.write(frame)   # 写入这一帧
+
+    out.write(frame)  # 写入这一帧
     frame_count += 1
 
     if frame_count >= max_frames:
+        print("截取完毕")
         break
 
 # 释放资源
